b06f4b934861a9167e31d8c89797e304ad7d2862
natuume <67637966+natuume@users.noreply.github.com>
2020-12-14 19:04:36 +0900

    修正

--- assessment.js
+++ assessment.js
@@ -5,24 +5,22 @@ const resultDivided = document.getElementById('result-area');
 const tweetDivided = document.getElementById('tweet-area');
 
 /**
- * 指定した要素の子どもを全て削除する
+ * 指定した要素の子どもを全て除去する
  * @param {HTMLElement} element HTMLの要素
  */
 function removeAllChildren(element) {
-  while (element.firstChild) {
-    // 子どもの要素があるかぎり削除
+  while (element.firstChild) { // 子どもの要素があるかぎり除去
     element.removeChild(element.firstChild);
   }
 }
 
 assessmentButton.onclick = () => {
   const userName = userNameInput.value;
-  if (userName.length === 0) {
-    // 名前が空の時は処理を終了する
+  if (userName.length === 0) { // 名前が空の時は処理を終了する
     return;
   }
 
-  // 診断結果の表示
+  // 診断結果表示エリアの作成
   removeAllChildren(resultDivided);
   const header = document.createElement('h3');
   header.innerText = '診断結果';
@@ -33,45 +31,26 @@ assessmentButton.onclick = () => {
   paragraph.innerText = result;
   resultDivided.appendChild(paragraph);
 
-  // ツイートボタンの表示
-  removeAllChildren(tweetDivided);//診断エリアの初期化
+  // ツイートエリアの作成
+  removeAllChildren(tweetDivided);
+  const anchor = document.createElement('a');
+  const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag='
+    + encodeURIComponent('あなたのいいところ')
+    + '&ref_src=twsrc%5Etfw';
+  anchor.setAttribute('href', hrefValue);
+  anchor.className = 'twitter-hashtag-button';
+  anchor.setAttribute('data-text', result);
+  anchor.innerText = 'Tweet #あなたのいいところ';
+  tweetDivided.appendChild(anchor);
 
-  //aタグを作って属性を設定する
-
-  const a = document.createElement('a');
-const hrefValue =
-  'https://twitter.com/intent/tweet?button_hashtag='
-  +encodeURIComponent('あなたのいいところ')
-  +'&ref_src=twsrc%5Etfw';
-a.setAttribute('href', hrefValue);
-a.className = 'twitter-hashtag-button';
-a.setAttribute('data-text', result);
-a.innerText = 'Tweet #あなたのいいところ';
-//*setAttributeのところ空白が1つ必要*
-tweetDivided.appendChild(a);
- 　
- //Tweetボタンの表示
-
-//aタグをHTMLとして追加する
- tweetDivided.appendChild(a);
-
-  // scriptタグをつくる
+  // widgets.js の設定
   const script = document.createElement('script');
   script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
-  //scriptタグをHTMLとして追加する
   tweetDivided.appendChild(script);
 };
 
-//入力欄でEnterキーを押したときに診断を実行する
-userNameInput.onkeydown = event => {
-  if (event.key === 'Enter') {
-    assessmentButton.onclick();
-  }
-    // TODO ボタンのonclick() 処理を呼び出す
-};
-
 const answers = [
-  '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
+  '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
   '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
   '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
@@ -86,8 +65,7 @@ const answers = [
   '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
   '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
   '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
-  '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
-  '{userName}のいいところは優しさです。あなたの優しい雰囲気や立ち振る舞いに多くの人が癒やされています。',
+  '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
 ];
 
 /**
@@ -97,23 +75,22 @@ const answers = [
  */
 function assessment(userName) {
   // 全文字のコード番号を取得してそれを足し合わせる
-  let sumOfCharCode = 0;
+  let sumOfcharCode = 0;
   for (let i = 0; i < userName.length; i++) {
-    sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
+    sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
   }
 
   // 文字のコード番号の合計を回答の数で割って添字の数値を求める
-  const index = sumOfCharCode % answers.length;
+  const index = sumOfcharCode % answers.length;
   let result = answers[index];
 
-  result = result.replace(/\{userName\}/g, userName);
+  result = result.replace(/{userName}/g, userName);
   return result;
 }
 
 // テストコード
 console.assert(
-  assessment('太郎') ===
-    '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+  assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
   '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
 );
 console.assert(
